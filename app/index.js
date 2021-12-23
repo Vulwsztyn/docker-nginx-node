09efeb9790716be3310bb5845af43b6ae933d0b1
Artur Mostowski <artur.mostowski@currency-one.com>
2021-12-24 00:29:22 +0100

    subdomains

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const msg = process.env.MSG || 'Hello World!'
+app.get('*', (req, res) => {
+  res.send(msg)
+})
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
